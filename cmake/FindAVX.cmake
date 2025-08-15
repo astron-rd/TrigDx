da477cbde7234e66f05988aab60883ee95df8fb7
Bram Veenboer <bram.veenboer@gmail.com>
2025-08-15 08:49:36 +0200

    Extend FindAVX

--- cmake/FindAVX.cmake
+++ cmake/FindAVX.cmake
@@ -1,26 +1,40 @@
 include(CheckCXXSourceRuns)
 
-set(SUPPORTED_COMPILERS Clang;GNU;Intel)
+set(SUPPORTED_COMPILERS Clang;GNU;Intel;IntelLLVM)
 
 if(CMAKE_CXX_COMPILER_ID IN_LIST SUPPORTED_COMPILERS)
-  set(CMAKE_REQUIRED_FLAGS "-mavx") # for GCC/Clang; use /arch:AVX for MSVC
+  if(CMAKE_CXX_COMPILER_ID STREQUAL "Intel")
+    set(CMAKE_REQUIRED_FLAGS "-xHost") # ICC
+  elseif(CMAKE_CXX_COMPILER_ID STREQUAL "IntelLLVM")
+    set(CMAKE_REQUIRED_FLAGS "-march=native") # ICX
+  else()
+    set(CMAKE_REQUIRED_FLAGS "-march=native") # GCC/Clang
+  endif()
 else()
-  message(FATAL_ERROR "Compiler : " ${CMAKE_CXX_COMPILER_ID} " not supported")
+  message(FATAL_ERROR "Unsupported compiler: ${CMAKE_CXX_COMPILER_ID}.")
 endif()
 
+# AVX check
 check_cxx_source_runs(
   "
 #include <immintrin.h>
 int main() {
-  __m256 a = _mm256_set_ps(-1.0f,2.0f,-3.0f,4.0f,-1.0f,2.0f,-3.0f,4.0f);
-  __m256 b = _mm256_set_ps(1.0f,2.0f,3.0f,4.0f,1.0f,2.0f,3.0f,4.0f);
-  __m256 result = _mm256_add_ps(a,b);
-  return 0;
-}"
+    __m256 a = _mm256_setzero_ps(); // AVX
+    (void) a;
+    return 0;
+}
+"
   HAVE_AVX)
 
-if(HAVE_AVX)
-  message(STATUS "AVX support: true")
-else()
-  message(STATUS "AVX support: false")
-endif()
+# AVX2 check
+check_cxx_source_runs(
+  "
+#include <immintrin.h>
+int main() {
+    __m256i a = _mm256_set1_epi32(-1);
+    __m256i b = _mm256_abs_epi32(a); // AVX2
+    (void) b;
+    return 0;
+}
+"
+  HAVE_AVX2)
