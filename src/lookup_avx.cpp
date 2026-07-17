200446cc60d0a524dbc4036737712855296b61db
mmancini-skao <Mattia.Mancini@skao.int>
2026-07-17 15:33:02 +0100

    Remove unused constant in lookup_avx.cpp
    Remove unused quarter_pi constant

--- src/lookup_avx.cpp
+++ src/lookup_avx.cpp
@@ -89,7 +89,6 @@ template <std::size_t NR_SAMPLES> struct LookupAVXBackend<NR_SAMPLES>::Impl {
     constexpr std::size_t VL = 8; // AVX processes 8 floats
     const __m256 scale = _mm256_set1_ps(SCALE);
     const __m256i mask = _mm256_set1_epi32(MASK);
-    const __m256i quarter_pi = _mm256_set1_epi32(NR_SAMPLES / 4);
 
     std::size_t i = 0;
     for (; i + VL <= n; i += VL) {
